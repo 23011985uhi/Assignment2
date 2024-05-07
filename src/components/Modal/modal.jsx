93ba498670f844ab83d2b73e50a627ac8b33be0b
Graeme <23011985uhi@gmail.com>
2024-05-07 12:10:44 +0100

    Hintsection popup modal implemented and functional

---
+++ src/components/Modal/modal.jsx
@@ -0,0 +1,23 @@
+import './Modal.css'; 
+import React from 'react'
+
+function Modal  ({ isOpen, onClose }) {
+  if (!isOpen) return null;
+
+  const closeModal = () => {
+    onClose();
+  };
+  
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+      <button className="close-btn" onClick={closeModal}>X</button>
+        <h2>This is a modal</h2>
+        <p>Modal content goes here...</p>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/components/pages/hintSection.jsx
+++ src/components/pages/hintSection.jsx
@@ -1,4 +1,18 @@
+import {useState} from 'react'
+import Modal from '../Modal/modal'
+
 function HintSection() {
+  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="col-sm">
       <h3 className="text-center">Hints</h3>
@@ -7,35 +21,36 @@ function HintSection() {
           <div className="col-6 text-center">
             <p>General</p>
             <div className="mb-5 d-grid gap-2">
-              <button type="button" className="btn btn-primary mb-1 p-2">Video</button>
-              <button type="button" className="btn btn-primary mb-1 p-2">Summary</button>
+              <button type="button" className="btn btn-primary mb-1 p-2" onClick={openModal}>Video</button>
+              <button type="button" className="btn btn-primary mb-1 p-2" onClick={openModal}>Summary</button>
             </div>
             <div className="mb-5 d-grid gap-2">
-              <button type="button" className="btn btn-primary mb-1 p-2">Video</button>
-              <button type="button" className="btn btn-primary mb-1 p-2">Summary</button>
+              <button type="button" className="btn btn-primary mb-1 p-2" onClick={openModal}>Video</button>
+              <button type="button" className="btn btn-primary mb-1 p-2" onClick={openModal}>Summary</button>
             </div>
             <div className="mb-5 d-grid gap-2">
-              <button type="button" className="btn btn-primary mb-1 p-2">Video</button>
-              <button type="button" className="btn btn-primary mb-1 p-2">Summary</button>
+              <button type="button" className="btn btn-primary mb-1 p-2" onClick={openModal}>Video</button>
+              <button type="button" className="btn btn-primary mb-1 p-2" onClick={openModal}>Summary</button>
             </div>
           </div>
           <div className="col-6 text-center">
             <p>Problem Specific</p>
             <div className="mb-5 d-grid gap-2">
-              <button type="button" className="btn btn-success mb-1 p-2">Video</button>
-              <button type="button" className="btn btn-success mb-1 p-2">Summary</button>
+              <button type="button" className="btn btn-success mb-1 p-2" onClick={openModal}>Video</button>
+              <button type="button" className="btn btn-success mb-1 p-2" onClick={openModal}>Summary</button>
             </div>
             <div className="mb-5 d-grid gap-2">
-              <button type="button" className="btn btn-success mb-1 p-2">Video</button>
-              <button type="button" className="btn btn-success mb-1 p-2">Summary</button>
+              <button type="button" className="btn btn-success mb-1 p-2" onClick={openModal}>Video</button>
+              <button type="button" className="btn btn-success mb-1 p-2" onClick={openModal}>Summary</button>
             </div>
             <div className="mb-5 d-grid gap-2">
-              <button type="button" className="btn btn-success mb-1 p-2">Video</button>
-              <button type="button" className="btn btn-success mb-1 p-2">Summary</button>
+              <button type="button" className="btn btn-success mb-1 p-2" onClick={openModal}>Video</button>
+              <button type="button" className="btn btn-success mb-1 p-2" onClick={openModal}>Summary</button>
             </div>
           </div>
         </div>
       </div>
+      <Modal isOpen={isModalOpen} onClose={closeModal} />
     </div>
   );
 };
