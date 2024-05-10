b3f4f9003284599e986129d4596eb9686f108631
Graeme <23011985uhi@gmail.com>
2024-05-10 13:21:38 +0100

    Multiple choice % attempts working now

--- src/components/Answers/answer5.jsx
+++ src/components/Answers/answer5.jsx
@@ -1,12 +1,14 @@
 import {db} from '../../services/firebase'
 //console.log(db)
-import { doc, getDoc } from "firebase/firestore"; 
+import { doc, getDoc, onSnapshot } from "firebase/firestore"; 
 import {useState, useEffect} from 'react';
+import './answer5.css'
 
 
-
-const Answer5 = ({ onCheckAnswer, resetButton }) => {
+const Answer5 = ({ onCheckAnswer, resetButton, answerKey, checkAnswer }) => {
   const [answer, setAnswer] = useState({ text: "", correct: false });
+  const [attempts, setAttempts] = useState(null);
+  const [totalAttempts, setTotalAttempts] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,15 +31,63 @@ const Answer5 = ({ onCheckAnswer, resetButton }) => {
     fetchData();
   }, []);
 
-  const checkAnswer = () => {
-    onCheckAnswer(answer.correct);
+  useEffect(() => {
+    const docRef = doc(db, "Hints", "Balances");
+  
+    const fetchData = async () => {
+      try {
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const hintData = docSnap.data();
+          const attempts = hintData.answer5 || 0;
+          const totalAttempts = hintData.undefined || 0;
+          setAttempts(attempts);
+          setTotalAttempts(totalAttempts);
+  
+          console.log(attempts);
+          console.log(totalAttempts);
+        } else {
+          console.log("No such document!");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+  
+    fetchData();
+  
+    // Add listener for document changes
+    const unsubscribe = onSnapshot(docRef, (doc) => {
+      if (doc.exists()) {
+        const hintData = doc.data();
+        const attempts = hintData.answer5 || 0;
+        const totalAttempts = hintData.undefined || 0;
+        setAttempts(attempts);
+        setTotalAttempts(totalAttempts);
+      } else {
+        console.log("No such document!");
+      }
+    });
+  
+    // Clean up listener
+    return () => unsubscribe();
+
+  }, []);
+
+  const percentage = attempts && totalAttempts ? (attempts / totalAttempts) * 100 : null;
+
+  const handleAnswerClick = () => {
+    onCheckAnswer(answer.correct, answerKey);
     resetButton(); // Call the resetButton function
+    checkAnswer(answer.correct);
   };
   
   return (
     <div>
-        <button type="button" className="btn btn-secondary mb-2 p-4 w-100 d-block " onClick={checkAnswer}>{answer.text}</button>
-      {/* You can render other components based on answer.correct if needed */}
+     <button type="button" className="btn btn-secondary mb-2 p-4 w-100 d-block relative-button" onClick={handleAnswerClick}>
+        <span className="button-content">{answer.text}</span>
+        <span className="additional-text">{percentage ? `${percentage.toFixed(0)}%` : ''}</span>
+      </button>
     </div>
   );
 };
--- src/components/pages/answerSection.jsx
+++ src/components/pages/answerSection.jsx
@@ -14,7 +14,7 @@ function AnswerSection() {
 
   const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
   const [checkButtonClassName, setCheckButtonClassName] = useState("btn btn-primary p-4");
-  const [checkButtonText, setCheckButtonText] = useState("Check My Answer");
+  const [checkButtonText, setCheckButtonText] = useState("Select an Answer");
   
 
 
@@ -62,20 +62,20 @@ function AnswerSection() {
       <div className="p-3 mb-2 bg-light">
         <div className="row">
           <div className="col-sm-6 d-grid gap-2">
-            <Answer1 onCheckAnswer={(isCorrect) => checkAnswer(isCorrect, 'answer1')} resetButton={resetButton} />
-            <Answer3 onCheckAnswer={(isCorrect) => checkAnswer(isCorrect, 'answer3')} resetButton={resetButton}/>
-            <Answer5 onCheckAnswer={(isCorrect) => checkAnswer(isCorrect, 'answer5')} resetButton={resetButton}/>
+            <Answer1 onCheckAnswer={(isCorrect) => checkAnswer(isCorrect, 'answer1')} resetButton={resetButton} checkAnswer={checkAnswer} answerKey="answer1"/>
+            <Answer3 onCheckAnswer={(isCorrect) => checkAnswer(isCorrect, 'answer3')} resetButton={resetButton} checkAnswer={checkAnswer} answerKey="answer3"/>
+            <Answer5 onCheckAnswer={(isCorrect) => checkAnswer(isCorrect, 'answer5')} resetButton={resetButton} checkAnswer={checkAnswer} answerKey="answer5"/>
           </div>
           <div className="col-sm-6 d-grid gap-2">
-            <Answer2 onCheckAnswer={(isCorrect) => checkAnswer(isCorrect, 'answer2')} resetButton={resetButton}/>
-            <Answer4 onCheckAnswer={(isCorrect) => checkAnswer(isCorrect, 'answer4')} resetButton={resetButton}/>
+            <Answer2 onCheckAnswer={(isCorrect) => checkAnswer(isCorrect, 'answer2')} resetButton={resetButton} checkAnswer={checkAnswer} answerKey="answer2"/>
+            <Answer4 onCheckAnswer={(isCorrect) => checkAnswer(isCorrect, 'answer4')} resetButton={resetButton} checkAnswer={checkAnswer} answerKey="answer4"/>
             <button className='btn btn-link mb-2 p-4 w-100 d-block' disabled></button>
             <div></div>
           </div>
         </div>
         <div className="row">
           <div className="col-sm text-center align-self-center">
-            <button type="button" className={`${checkButtonClassName}`} onClick={() => checkAnswer(isAnswerCorrect)}>
+            <button type="button" className={`${checkButtonClassName}`} >
               {checkButtonText}
             </button>
           </div>
